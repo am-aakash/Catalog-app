cc1e2a547ba0f7a5d9649b7fef4aff5cf9b43901
Aakash Chaudhary <64517711+am-aakash@users.noreply.github.com>
2021-05-18 03:37:47 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
-import 'package:basic_project/home_page.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:basic_project/pages/home_page.dart';
+import 'package:basic_project/pages/login_page.dart';
+import 'package:basic_project/utils/routes.dart';
+//import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart'; //GoogleFonts
 
 void main() {
   runApp(MyApp());
@@ -10,7 +13,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      //home: HomePage(),
+      // themeMode: ThemeMode.dark,
+      // darkTheme: ThemeData(brightness: Brightness.dark),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        //primaryTextTheme: GoogleFonts.latoTextTheme(),
+      ),
+      initialRoute: MyRoutes.homeRoute,
+      routes: {
+        "/": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
+      },
     );
   }
 }
